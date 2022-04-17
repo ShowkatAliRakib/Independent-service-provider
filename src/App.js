31dc526472cd2fab8f0ee01dc130ce8b73ea5494
ShowkatAliRakib <rakibmd54496313@gmail.com>
2022-04-18 00:07:30 +0600

    navbar-added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import Header from './Components/Shared/Header/Header';
 import Footer from './Components/Shared/Footer/Footer';
+import Home from './Components/Pages/Home/Home';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Routes>
-        {/* <Route path='/' element={}></Route>
-        <Route path='/' element={}></Route>
-        <Route path='/' element={}></Route>
-        <Route path='/' element={}></Route> */}
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        {/* <Route path='/' element={}></Route> */}
+        
       </Routes>
       <Footer></Footer>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
 
-ReactDOM.render(
+
+import React from 'react'
+import ReactDOMClient from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter } from 'react-router-dom'
+
+const root = ReactDOMClient.createRoot(document.getElementById('root'))
+
+root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
