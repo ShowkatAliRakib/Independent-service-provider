b915dd4c512b9426728fba4b2944d3127358bc18
ShowkatAliRakib <rakibmd54496313@gmail.com>
2022-04-18 00:48:40 +0600

    banner-added

--- src/Components/Pages/Banner/Banner.js
+++ src/Components/Pages/Banner/Banner.js
@@ -1,37 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Banner.css'
+import doctor1 from '../../../image/doctor1.png'
+import doctor2 from '../../../image/doctor2.png'
+import doctor3 from '../../../image/doctor3.png'
+import { Carousel } from 'react-bootstrap';
 
 const Banner = () => {
+    const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
     return (
-        <div>
-            <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      {/* <img src={}className="d-block w-100" alt="..."> */}
-    </div>
-    <div className="carousel-item">
-      {/* <img src="..." className="d-block w-100" alt="..."> */}
-    </div>
-    <div className="carousel-item">
-      {/* <img src="..." className="d-block w-100" alt="..."> */}
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
+        
+        <div className="container">
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+<Carousel.Item>
+<img
+className="d-block w-100"
+src={doctor1}
+alt="First slide"
+/>
+<Carousel.Caption>
+<h3>First slide label</h3>
+<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+</Carousel.Caption>
+</Carousel.Item>
+<Carousel.Item>
+<img
+className="d-block w-100"
+src={doctor2}
+alt="Second slide"
+/>
+
+<Carousel.Caption>
+<h3>Second slide label</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+</Carousel.Caption>
+</Carousel.Item>
+<Carousel.Item>
+<img
+className="d-block w-100"
+src={doctor3}
+alt="Third slide"
+/>
+
+<Carousel.Caption>
+<h3>Third slide label</h3>
+<p>
+Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+</p>
+</Carousel.Caption>
+</Carousel.Item>
+</Carousel>
 </div>
-        </div>
+        
     );
-};
 
 export default Banner;
