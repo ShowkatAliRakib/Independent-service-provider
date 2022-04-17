5d004912a2fcd341f5f4f709cbd4eab4029a3549
ShowkatAliRakib <rakibmd54496313@gmail.com>
2022-04-17 23:21:17 +0600

    components-setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './Components/Shared/Header/Header';
+import Footer from './Components/Shared/Footer/Footer';
 
 function App() {
   return (
     <div>
-      
+      <Header></Header>
+      <Routes>
+        {/* <Route path='/' element={}></Route>
+        <Route path='/' element={}></Route>
+        <Route path='/' element={}></Route>
+        <Route path='/' element={}></Route> */}
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
